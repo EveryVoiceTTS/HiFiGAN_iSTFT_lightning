02fb9b3ca4e87050fea75602a6f0aec8c35c878e
Aidan Pine <hello@aidanpine.ca>
2022-11-04 21:50:34 +0000

    refactor(cli): factor out config override

--- hfgl/cli.py
+++ hfgl/cli.py
@@ -1,17 +1,15 @@
-import json
 import os
 from enum import Enum
 from pathlib import Path
 from typing import List, Optional
 
 import typer
-import yaml
 from loguru import logger
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 from pytorch_lightning.loggers import TensorBoardLogger
 from smts.preprocessor import Preprocessor
-from smts.utils import expand_config_string_syntax
+from smts.utils import update_config_from_cli_args, update_config_from_path
 
 from hfgl.config import CONFIGS, HiFiGANConfig
 from hfgl.dataset import HiFiGANDataModule
@@ -61,54 +59,41 @@ def train(
     accelerator: str = typer.Option("auto"),
     devices: str = typer.Option("auto"),
     strategy: str = typer.Option(None),
-    config: List[str] = typer.Option(None),
+    config_args: List[str] = typer.Option(None, "--config", "-c"),
     config_path: Path = typer.Option(None, exists=True, dir_okay=False, file_okay=True),
 ):
     original_config = CONFIGS[name.value]
-    if config is not None and config:
-        for update in config:
-            key, value = update.split("=")
-            logger.info(f"Updating config '{key}' to value '{value}'")
-            original_config = original_config.update_config(
-                expand_config_string_syntax(update)
-            )
-    updated_config: HiFiGANConfig = original_config
-    if config_path is not None:
-        logger.info(f"Loading and updating config from '{config_path}'")
-        with open(config_path, "r") as f:
-            config_override = (
-                json.load(f) if config_path.suffix == ".json" else yaml.safe_load(f)
-            )
-        updated_config = updated_config.update_config(config_override)
-    tensorboard_logger = TensorBoardLogger(**(updated_config.training.logger.dict()))
+    config: HiFiGANConfig = update_config_from_cli_args(config_args, original_config)
+    config = update_config_from_path(config_path, config)
+    tensorboard_logger = TensorBoardLogger(**(config.training.logger.dict()))
     lr_monitor = LearningRateMonitor(logging_interval="step")
     logger.info("Starting training for HiFiGAN model.")
     ckpt_callback = ModelCheckpoint(
         monitor="validation/mel_spec_error",
         mode="min",
         save_last=True,
-        save_top_k=updated_config.training.save_top_k_ckpts,
-        every_n_train_steps=updated_config.training.ckpt_steps,
-        every_n_epochs=updated_config.training.ckpt_epochs,
+        save_top_k=config.training.save_top_k_ckpts,
+        every_n_train_steps=config.training.ckpt_steps,
+        every_n_epochs=config.training.ckpt_epochs,
     )
     trainer = Trainer(
         logger=tensorboard_logger,
         accelerator=accelerator,
         devices=devices,
-        max_epochs=updated_config.training.max_epochs,
+        max_epochs=config.training.max_epochs,
         callbacks=[ckpt_callback, lr_monitor],
         strategy=strategy,
         detect_anomaly=False,  # used for debugging, but triples training time
     )
-    vocoder = HiFiGAN(updated_config)
-    data = HiFiGANDataModule(updated_config)
+    vocoder = HiFiGAN(config)
+    data = HiFiGANDataModule(config)
     last_ckpt = (
-        updated_config.training.finetune_checkpoint
-        if updated_config.training.finetune_checkpoint is not None
-        and os.path.exists(updated_config.training.finetune_checkpoint)
+        config.training.finetune_checkpoint
+        if config.training.finetune_checkpoint is not None
+        and os.path.exists(config.training.finetune_checkpoint)
         else None
     )
-    tensorboard_logger.log_hyperparams(updated_config.dict())
+    tensorboard_logger.log_hyperparams(config.dict())
     trainer.fit(vocoder, data, ckpt_path=last_ckpt)
 
 
