0649eff640933fb4a1fece0a2b48b0ed05ef5ad3
Eric Joanis <Eric.Joanis@cnrc-nrc.gc.ca>
2024-01-17 11:06:08 -0500

    refactor: use Python 3.10 style typing for unions and basic types
    Since Python 3.10, we can use A | B instead of Union[A, B]
    
    Since Python 3.9, typing.List, .Dict, .Tuple, .Set etc are deprecated because
    the builtin types now support indexing.

--- hfgl/model.py
+++ hfgl/model.py
@@ -1,6 +1,5 @@
 import itertools
 import math
-from typing import Dict, Union
 
 import pytorch_lightning as pl
 import torch
@@ -441,7 +440,7 @@ class MultiScaleDiscriminator(torch.nn.Module):
 
 
 class HiFiGAN(pl.LightningModule):
-    def __init__(self, config: Union[Dict, VocoderConfig]):
+    def __init__(self, config: dict | VocoderConfig):
         super().__init__()
         self.automatic_optimization = False
         # Because we serialize the configurations when saving checkpoints,
@@ -452,9 +451,9 @@ class HiFiGAN(pl.LightningModule):
         self.mpd = MultiPeriodDiscriminator(config)
         self.msd = MultiScaleDiscriminator(config)
         self.generator = Generator(config)
-        self.batch_size = (
-            self.config.training.batch_size
-        )  # this is declared explicitly so that auto_scale_batch_size works: https://pytorch-lightning.readthedocs.io/en/stable/advanced/training_tricks.html
+        # batch_size is declared explicitly so that auto_scale_batch_size works:
+        # https://pytorch-lightning.readthedocs.io/en/stable/advanced/training_tricks.html
+        self.batch_size = self.config.training.batch_size
         self.save_hyperparameters()  # TODO: ignore=['specific keys'] - I should ignore some unnecessary/problem values
         self.audio_config = config.preprocessing.audio
         self.sampling_rate_change = (
@@ -548,7 +547,8 @@ class HiFiGAN(pl.LightningModule):
             )
         else:
             scheduler_g = torch.optim.lr_scheduler.ExponentialLR(
-                optim_g, gamma=0.999  # TODO: parametrize this
+                optim_g,
+                gamma=0.999,  # TODO: parametrize this
             )
             scheduler_d = torch.optim.lr_scheduler.ExponentialLR(optim_d, gamma=0.999)
             return [optim_g, optim_d], [scheduler_g, scheduler_d]
