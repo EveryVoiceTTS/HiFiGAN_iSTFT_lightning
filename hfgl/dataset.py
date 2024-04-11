6e939de9fb7e164c672d55baa424362cca6684db
MENGZHEGENG <gmzcuhk@gmail.com>
2024-04-11 16:14:04 +0000

    ci: turn wav path to str

--- hfgl/dataset.py
+++ hfgl/dataset.py
@@ -41,15 +41,17 @@ class SpecDataset(Dataset):
         speaker = "default" if "speaker" not in item else item["speaker"]
         language = "default" if "language" not in item else item["language"]
         y, _ = torchaudio.load(
-            self.preprocessed_dir
-            / "audio"
-            / self.sep.join(
-                [
-                    item["basename"],
-                    speaker,
-                    language,
-                    f"audio-{self.output_sampling_rate}.wav",
-                ]
+            str(
+                self.preprocessed_dir
+                / "audio"
+                / self.sep.join(
+                    [
+                        item["basename"],
+                        speaker,
+                        language,
+                        f"audio-{self.output_sampling_rate}.wav",
+                    ]
+                )
             )
         )
         y = (
